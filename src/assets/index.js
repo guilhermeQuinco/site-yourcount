49f8c580c23088f5809cd4efaf6eb520283eaa2b
guiguhguile <130392565+guiguhguile@users.noreply.github.com>
2024-08-06 09:14:38 -0400

    fix: remove import grid

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,5 @@
-import gridCard3 from "./cardgrid3.png";
 import socialmedia from "./SocialMedia.svg";
 import inbounding from "./Inbounding.svg";
 import website from "./Website.svg";
 
-export { gridCard3, socialmedia, inbounding, website };
+export { socialmedia, inbounding, website };
